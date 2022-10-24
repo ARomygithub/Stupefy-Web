d1e5f80a63635a5551e5504a3009c2e64dc13ed5
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2022-10-24 16:58:42 +0700

    add detail album front end without song list

---
+++ public/detail-album.php
@@ -0,0 +1,36 @@
+<?php
+    function echo_detail_album($title = "Album Title", $artist = "Artist", $total_duration = 0){
+        $secs = $total_duration%60;
+        $mins = intdiv($total_duration,60)%60;
+        $hour = intdiv($total_duration,3600);
+        $html = <<<"EOT"
+            <div id="detail-album">
+                <div style="background-color: white; aspect-ratio: 1; width: 200px; margin: 20px;"></div>
+                <div class="main-info">
+                    <div class="album-title">$title</div>
+                    <div class="artist">$artist</div>
+                    <div class="duration">$hour Hours $mins Minutes $secs Seconds</div>
+                </div>
+            </div>
+        EOT;
+
+        echo $html;
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Binotify</title>
+    <link rel="stylesheet" href="css/detail-album.css" />
+</head>
+<body>
+    <div id="container">
+        <?php echo_detail_album("4 phenomena", "Photon Maiden", 3098) ?>
+    </div>
+</body>
+</html>
