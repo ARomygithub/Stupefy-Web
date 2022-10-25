4d44181fcd013ca1899a01f92da204ff12d89d67
firizky29 <13520095@std.stei.itb.ac.id>
2022-10-25 20:14:30 +0700

    feat: creating view for home (still in progress, just created the skeleton)

--- app/database/migrations/create_table_users.php
+++ app/database/migrations/create_table_users.php
@@ -7,7 +7,7 @@ function create_table_users(){
     $db = new Database();
     $db->prepare('CREATE TABLE IF NOT EXISTS user(
         user_id INT NOT NULL AUTO_INCREMENT,
-        email VARCHAR(256) NOT NULL,
+        email VARCHAR(256) NOT NULL UNIQUE,
         password VARCHAR(256) NOT NULL,
         username VARCHAR(64) NOT NULL,
         isAdmin BOOLEAN NOT NULL,
