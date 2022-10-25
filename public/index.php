f452903cb467722ddf88d23d836899234c4af24c
firizky29 <13520095@std.stei.itb.ac.id>
2022-10-25 23:35:42 +0700

    feat: created sidebar and navbar

--- public/index.php
+++ public/index.php
@@ -5,14 +5,14 @@ session_start();
 require_once __DIR__ . '/../app/controllers/authorization.php';
 
 $nav = file_get_contents('./html/template/authorized-navbar.html');
-$sidebar = file_get_contents('./html/template/user-sidebar.html');
+$sidebar = file_get_contents('./html/template/admin-sidebar.html');
 
 $body = file_get_contents('./html/home.html');
 
 if(isset($_SESSION['user_id']) && $user_role === 'admin'){
     $sidebar = file_get_contents('./html/template/admin-sidebar.html');
 } else if(!isset($_SESSION['user_id'])){
-    $nav = file_get_contents('./html/template/unauthorized-navbar.html');  
+    $nav = file_get_contents('./html/template/authorized-navbar.html');  
 } 
 
 $body = str_replace('{{ nav }}', $nav, $body);
