9d88cc7dd236b7120e28301aa6f97ce4ba449385
firizky29 <13520095@std.stei.itb.ac.id>
2022-10-26 16:01:22 +0700

    adding add-album page view (still static)

---
+++ public/add-album.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+require_once __DIR__ . '/../app/controllers/AuthController.php';
+
+$nav = file_get_contents('./html/template/authorized-navbar.html');
+$sidebar = file_get_contents('./html/template/admin-sidebar.html');
+
+$body = file_get_contents('./html/add-album.html');
+
+if(!isset($_SESSION['user_id']) && $user_role === 'admin'){
+    $sidebar = file_get_contents('./html/template/admin-sidebar.html');
+    $body = str_replace('{{ nav }}', $nav, $body);
+    $body = str_replace('{{ sidebar }}', $sidebar, $body);
+
+    echo $body;
+} else{
+    http_response_code(403);
+    echo file_get_contents('./html/403.html');
+    // die('Forbidden');
+} 
+
+?>
