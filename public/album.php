5b6fb0aa4f407968c736761ced9249f00b0e7a43
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2022-10-27 07:11:50 +0700

    feat: add album.html

--- public/album.php
+++ public/album.php
@@ -19,26 +19,25 @@
         echo $html;
     }
 
-?>
+    session_start();
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Binotify</title>
-    <link rel="stylesheet" href="css/album.css" />
-</head>
-<body>
-    <div id="container">
-        <h1>Albums</h1>
-        <div id="content">
-            <?php 
-                echo_card("Mantra Hujan", "Kobo Kanaeru", "2022", "VTuber"); 
-                echo_card("Mantra Hujan", "Kobo Kanaeru", "2022", "VTuber"); 
-            ?>
-        </div>
-    </div>
-</body>
-</html>
+    require_once __DIR__ . '/../app/controllers/AuthController.php';
+    
+    $nav = file_get_contents('./html/template/authorized-navbar.html');
+    $sidebar = file_get_contents('./html/template/admin-sidebar.html');
+    
+    $body = file_get_contents('./html/album.html');
+    
+    if(!isset($_SESSION['user_id']) && $user_role === 'admin'){
+        $sidebar = file_get_contents('./html/template/admin-sidebar.html');
+        $body = str_replace('{{ nav }}', $nav, $body);
+        $body = str_replace('{{ sidebar }}', $sidebar, $body);
+    
+        echo $body;
+    } else{
+        http_response_code(403);
+        echo file_get_contents('./html/403.html');
+        // die('Forbidden');
+    } 
+    
+?>
