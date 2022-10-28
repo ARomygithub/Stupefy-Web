35fae7ac0612b359385554ebd87ad48333460aff
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-10-28 10:31:18 +0700

    feat: add event enter ke search bar

--- public/js/nav.js
+++ public/js/nav.js
@@ -13,3 +13,11 @@ function logout(){
     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
     xhr.send(formData);
 }
+
+let searchInput = document.getElementById("search-input");
+searchInput.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        location.href = "/public/search.php?search=" + this.value;
+    }
+});
