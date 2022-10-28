dc88b53c0460e86d998994adf874347f598076d0
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-10-28 10:39:14 +0700

    fix: logout pas search

--- public/js/search.js
+++ public/js/search.js
@@ -282,3 +282,20 @@ filter_genre.addEventListener("change", function() {
     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
     xhr.send();
 });
+
+logout = document.getElementsByClassName("logout")[0];
+logout.addEventListener("click", function() {
+    let xhr = new XMLHttpRequest();
+    formData = new FormData();
+    formData.append("logout", true);
+    xhr.onreadystatechange = function() { 
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.location.href = ".";
+        }
+    }
+    url = "/app/controllers/AuthController.php";
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhr.send(formData);    
+});
